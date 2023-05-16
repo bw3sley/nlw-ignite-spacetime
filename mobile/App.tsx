ef127461801083f2b6bd2ce5edc7a238a18da4b8
Wesley Bernardes <bw3sley@gmail.com>
2023-05-16 02:36:09 -0300

    :tada: chore: initial commit

---
+++ mobile/App.tsx
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className='bg-zin-900'>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
