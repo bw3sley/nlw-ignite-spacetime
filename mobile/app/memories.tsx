992cd1d51313516438ad253c6f89b875bb68ca78
Wesley Bernardes <bw3sley@gmail.com>
2023-05-22 20:48:14 -0300

    :sparkles: feat: create app pages

---
+++ mobile/app/memories.tsx
@@ -0,0 +1,49 @@
+import { ScrollView, TouchableOpacity, View } from 'react-native'
+
+import Icon from '@expo/vector-icons/Feather'
+
+import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
+
+import { Link, useRouter } from 'expo-router'
+
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+
+import * as SecureStore from 'expo-secure-store'
+
+export default function NewMemory() {
+  const { bottom, top } = useSafeAreaInsets()
+
+  const router = useRouter()
+
+  async function signOut() {
+    await SecureStore.deleteItemAsync('token')
+
+    router.push('/')
+  }
+
+  return (
+    <ScrollView
+      className="flex-1 px-8"
+      contentContainerStyle={{ paddingBottom: bottom, paddingTop: top }}
+    >
+      <View className="mt-4 flex-row items-center justify-between">
+        <NLWLogo />
+
+        <View className="flex-row gap-2">
+          <TouchableOpacity
+            onPress={signOut}
+            className="h-10 w-10 items-center justify-center rounded-full bg-red-500"
+          >
+            <Icon name="log-out" size={16} color="#000" />
+          </TouchableOpacity>
+
+          <Link href="/new" asChild>
+            <TouchableOpacity className="h-10 w-10 items-center justify-center rounded-full bg-green-500">
+              <Icon name="plus" size={16} color="#000" />
+            </TouchableOpacity>
+          </Link>
+        </View>
+      </View>
+    </ScrollView>
+  )
+}
